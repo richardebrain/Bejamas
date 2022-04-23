91e478343e6f6d9baad8a9a9c6f949f346af0b88
richardebrain <richardebrain20@gmail.com>
2022-04-23 05:49:12 +0100

    fixed onchange error on checkboc component

--- src/components/check-box/checkbox.tsx
+++ src/components/check-box/checkbox.tsx
@@ -3,7 +3,7 @@ import "./checkbox.scss";
 const Checkbox = ({ ...otherprops }) => {
   return (
     <label className="container">
-      <input type="checkbox" {...otherprops} className="checkbox" onChange={''}/>
+      <input type="checkbox" {...otherprops} className="checkbox" />
       <span className="checkbox-mark"></span>
     </label>
   );
